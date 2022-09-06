0947a3a2b8c751b66cd27bed8ea0bd8eaadebda7
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-09-05 18:19:09 -0700

    added functionality for getting value of selected answer when clicking submit input

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-// add eventlistener to change display style when object hits
+// add eventlistener? to change class and display next question
 
-let pleaseWork = document.createElement("h1")
-pleaseWork.innerHTML = "amongus"
-document.body.append(pleaseWork)
+
+//get and store the answer that the user chooses
+document.getElementById('submit').addEventListener('click', function(event){
+    event.preventDefault();
+    let selectedAnswer = document.getElementsByName('answer');
+    // console.log(selectedAnswer)
+    for(i=0; i < selectedAnswer.length; i++){
+        if(selectedAnswer[i].checked){
+            // console.log(selectedAnswer[i].value)
+            return selectedAnswer[i].value
+        }
+    }
+    document.getElementById('answer1')
+})
+
